60344dbec41b582e32b21673512d103bb463dde2
Victoria Brouard <victoria_brouard@hotmail.co.uk>
2015-12-08 17:09:21 +0000

    Add Header

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Quizone::Application.routes.draw do
+
+  root 'homes#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
